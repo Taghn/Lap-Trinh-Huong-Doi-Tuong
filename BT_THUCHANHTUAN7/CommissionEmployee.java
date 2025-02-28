79bccb0a5b3f1fe2b6de12fd1ff91b4f93d761bc
Taghn <sutheo7514@gmail.com>
2025-02-28 17:35:32 +0700

    tuan 7

---
+++ BT_THUCHANHTUAN7/CommissionEmployee.java
@@ -0,0 +1,49 @@
+public class CommissionEmployee extends Employee{
+    protected  double grossSales;
+    protected  double commissionRate;
+
+    public CommissionEmployee(String firstName, String lastName, String SecurityNumber, double grossSales, double commissionRate){
+        super(firstName,lastName,SecurityNumber); 
+        if(grossSales<0.0){
+            throw new IllegalArgumentException("Doanh so phai >= 0.0");
+        }
+        if(commissionRate<= 0.0|| commissionRate >= 1.0){
+            throw new IllegalArgumentException("Ty le hoa hong > 0.0 va < 1.0");
+        }
+        this.grossSales=grossSales;
+        this.commissionRate=commissionRate;
+    }
+    public void setGrossSales(double grossSales){
+        if(grossSales < 0.0){
+            throw new IllegalArgumentException("Doanh so phai >= 0.0");
+        }
+        this.grossSales=grossSales;
+    }
+
+    public double getGrossSales(){
+        return grossSales;
+    }
+
+    public void setCommissionRate(double commissionRate){
+        if(commissionRate <= 0.0 || commissionRate >= 1.0){
+            throw new IllegalArgumentException("Ty le hoa hong > 0.0 va < 1.0");
+        }
+        this.commissionRate=commissionRate;
+    }
+
+    public double getCommissionRate(){
+        return commissionRate;
+    }
+    @Override
+    public double earnings(){
+        return getCommissionRate()*getGrossSales();
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s:%s %s%n%s: $%,.2f; %s: %.2f", 
+        "commission employee",super.toString(),
+        "gross sales",getGrossSales(),
+        "commission rate", getCommissionRate());
+    }
+}
---
+++ BT_THUCHANHTUAN7/PayrollSystemTest.java
@@ -0,0 +1,35 @@
+public class PayrollSystemTest {
+    public static void main(String[] args) {
+        SalariedEmployee salariedEmployee = new SalariedEmployee("Nguyen", "Huu","111-11-1111", 800.000);
+        HourlyEmployee hourlyEmployee = new HourlyEmployee("nguyen","huu","222-22-2222", 16.75,40);
+        CommissionEmployee commissionEmployee = new CommissionEmployee("Nguyen", "Duc", "333-33-3333", 100000, .06);
+        BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee("Nguyen", "Toan", "444-44-4444", 5000, .04, 300);
+        System.out.println("Nhan vien se dc xu ly doc lap: ");
+        System.out.printf("%n%s%n%s: $%.2f%n%n",salariedEmployee,"earned",salariedEmployee.earnings());
+        System.out.printf("%s%n%s: $%,.2f%n%n",hourlyEmployee,"earned",hourlyEmployee.earnings());
+        System.out.printf("%s%n%s: $%,.2f%n%n",commissionEmployee,"earned",commissionEmployee.earnings());
+        System.out.printf("%s%n%s: $%,.2f%n%n",basePlusCommissionEmployee,"earned",basePlusCommissionEmployee.earnings());
+
+        Employee[] employees = new Employee[4];
+        employees[0]=salariedEmployee;
+        employees[1]=hourlyEmployee;
+        employees[2]=commissionEmployee;
+        employees[3]=basePlusCommissionEmployee;
+
+        System.out.println("--------------------------");
+        System.out.println("Nhan vien se duoc xu ly da hinh Polymorphism:");
+        for(Employee currentEmployee: employees){
+            System.out.println(currentEmployee);
+            if(currentEmployee instanceof BasePlusCommissionEmployee){
+                BasePlusCommissionEmployee employee = (BasePlusCommissionEmployee) currentEmployee;
+                employee.setBaseSalary(1.10*employee.getBaseSalary());
+                System.out.printf("Nhan vien duoc thuong voi 10%% la: $%,.2f%n",employee.getBaseSalary());
+            }
+            System.out.printf("Thu nhap: $%,.2f%n%n", currentEmployee.earnings());
+        }
+        for(int j = 0; j<employees.length;j++){
+            System.out.printf("Nhan vien %d thuoc dang %s%n",j,employees[j].getClass().getName());
+        }
+
+    }
+}
